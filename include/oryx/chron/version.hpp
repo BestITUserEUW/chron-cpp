14d48eb85928b7803414cd8822f21e8859955c0d
BestITUserEUW <phile.benner@gmail.com>
2025-10-12 02:12:15 +0200

    version bump

--- include/oryx/chron/version.hpp
+++ include/oryx/chron/version.hpp
@@ -5,7 +5,7 @@
 namespace oryx::chron {
 
 inline constexpr int kVersionMajor = 0;
-inline constexpr int KVersionMinor = 2;
+inline constexpr int KVersionMinor = 3;
 inline constexpr int kVersionPatch = 0;
 
 auto MakeStringVersion() -> std::string;
