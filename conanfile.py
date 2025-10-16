7a9ff4c7a9db1e3ba8f8658671fb0765cc9cdca2
BestITUserEUW <phile.benner@gmail.com>
2025-10-17 00:02:47 +0200

    finished conan file

--- conanfile.py
+++ conanfile.py
@@ -1,53 +1,70 @@
 from conan import ConanFile
-from conan.tools.files import get, copy, rmdir
-from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
-from conan.tools.env import VirtualBuildEnv
 from conan.tools.build import check_min_cppstd
-
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+from conan.tools.files import (
+    apply_conandata_patches,
+    copy,
+    export_conandata_patches,
+    get,
+    rm,
+    rmdir,
+)
 import os
 
-required_conan_version = ">=2.18.1"
+
+required_conan_version = ">=2.0.9"
+
+#
+# INFO: Please, remove all comments before pushing your PR!
+#
 
 
 class ChronCppConan(ConanFile):
-    name = "cron-cpp"
-    description = "cron-cpp is a C++20 scheduling library using cron formatting"
-    license = "UNLICENSE"
+    name = "chron-cpp"
+    version = "0.5.0"
+    description = "C++20 chron format scheduling library"
+    license = "Unlicense"
     url = "https://github.com/conan-io/conan-center-index"
     homepage = "https://github.com/BestITUserEUW/chron-cpp"
     topics = "chron"
-    generators = "cmake"
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
-
     options = {
         "shared": [True, False],
+        "fPIC": [True, False],
     }
-
     default_options = {
         "shared": False,
+        "fPIC": True,
     }
+    # In case having config_options() or configure() method, the logic should be moved to the specific methods.
+    implements = ["auto_shared_fpic"]
 
-    def build_requirements(self):
-        self.tool_requires("cmake/[>=3.23 <4]")
+    # no exports_sources attribute, but export_sources(self) method instead
+    def export_sources(self):
+        export_conandata_patches(self)
 
-    def validate(self):
-        if self.settings.get_safe("compiler.cppstd"):
-            check_min_cppstd(self, self._min_cppstd)
+    def configure(self):
+        if self.options.shared:
+            self.options.rm_safe("fPIC")
 
     def layout(self):
-        cmake_layout(self, src_folder=".")
+        cmake_layout(self)
+
+    def validate(self):
+        check_min_cppstd(self, 20)
+
+    def build_requirements(self):
+        self.tool_requires("cmake/[>=3.24]")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        apply_conandata_patches(self)
 
     def generate(self):
-        env = VirtualBuildEnv(self)
-        env.generate()
         deps = CMakeDeps(self)
         deps.generate()
         tc = CMakeToolchain(self)
-        tc.cache_variables["ORYX_CHRON_BUILD_SHARED_LIBS"] = self.options.shared
         tc.generate()
 
     def build(self):
@@ -58,27 +75,22 @@ class ChronCppConan(ConanFile):
     def package(self):
         copy(
             self,
-            pattern="LICENSE",
-            dst=os.path.join(self.package_folder, "licenses"),
-            src=self.source_folder,
+            "LICENSE",
+            self.source_folder,
+            os.path.join(self.package_folder, "licenses"),
         )
         cmake = CMake(self)
         cmake.install()
+
+        # Some files extensions and folders are not allowed. Please, read the FAQs to get informed.
+        # Consider disabling these at first to verify that the package_info() output matches the info exported by the project.
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
+        rmdir(self, os.path.join(self.package_folder, "share"))
+        rm(self, "*.pdb", self.package_folder, recursive=True)
 
     def package_info(self):
+        # library name to be packaged
         self.cpp_info.libs = ["chron-cpp"]
-
-    @property
-    def _min_cppstd(self):
-        return 20
-
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "Visual Studio": "17",
-            "msvc": "1938",
-            "gcc": "13",
-            "clang": "16",
-            "apple-clang": "16",
-        }
+        self.cpp_info.set_property("cmake_file_name", "chron-cpp")
+        self.cpp_info.set_property("cmake_target_name", "oryx::chron-cpp")
